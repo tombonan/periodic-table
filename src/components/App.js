12f8a34071cc7c277baaadd3269ef5563ad4d60a
tombonan <tombonan018@gmail.com>
2019-01-28 12:45:33 -0700

    Move around components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <h1>Periodic Table of Elements</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
