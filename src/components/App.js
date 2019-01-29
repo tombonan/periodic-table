a72e2b1a402c73d09ea98b609b5afbb0dcf72290
tombonan <tombonan018@gmail.com>
2019-01-28 20:32:26 -0700

    Finish styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,22 @@ import { elementsList } from "../json/elements.js";
 class App extends Component {
     render() {
         const data = elementsList;
-        const listElements = data.map((elem, index) =>
-                                      <Element symbol={elem.symbol} num = {index + 1}/>
+        const elemListOne = data.slice(0,57);
+        const elemListTwo = data.slice(71,88);
+        const elemListThree = data.slice(102, data.length);
+        
+        const lancthanides = data.slice(57,70);
+        const actinides = data.slice(89,102);
+        const elemList = elemListOne.concat(elemListTwo, elemListThree, lancthanides, actinides);
+        const elements = elemList.map((elem, index) =>
+                                      <Element symbol={elem.symbol} id = {elem.number} key = {elem.number} name = {elem.name} category = {elem.category}/>
                                      );
     return (
         <div className="wrapper">
           <h1>Periodic Table of Elements</h1>
-          <div className = "table">{ listElements }</div>
+          <div className = "table">
+            { elements }
+          </div>
       </div>
     );
   }
--- src/components/Element.js
+++ src/components/Element.js
@@ -4,25 +4,17 @@ class Element extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hover: false
         };
     }
 
-    onHover = (e) => {
-        this.setState(prevState => ({
-            hover: !prevState.hover
-        }));
-    };
-    
     render() {
         return(
             <div 
-              onMouseEnter={this.onHover.bind(this)}
-              onMouseLeave={this.onHover.bind(this)}
-              className = {`element element-${this.props.num} ${this.state.hover ? 'hover' : ''}`}
+              className = {`element element-${this.props.id} ${this.props.category}`}
             >
-              <p className = "number">{this.props.num}</p>
+              <p className = "number">{this.props.id}</p>
               <h3 className = "symbol">{this.props.symbol}</h3>
+              <p className = "name">{this.props.name}</p>
             </div>
         );
     }
